d24bfbd7c282a43b47324fb7eb00e7b1ab196eca
lindajhk <lindajiheekang@gmail.com>
2023-04-18 14:08:41 -0700

    Added restart functionality and fixed error received when no more items in list

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from tkinter import *
 import pandas
 import requests
 import random
-import os
+
 
 BASE_URL = "https://www.dictionaryapi.com/api/v3/references/medical/json/"
 BACKGROUND_COLOR = "#B1DDC6"
@@ -21,7 +21,17 @@ else:
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
-    current_card = random.choice(to_learn.iloc[:, 0])
+
+    try:
+        current_card = random.choice(to_learn.iloc[:, 0].tolist())
+    except IndexError:
+        canvas.itemconfig(card_title, text="Congrats!\nYou've learned all the cards!")
+        canvas.itemconfig(front_card_word, text="", fill="white")
+        unknown_button.grid_forget()
+        known_button.grid_forget()
+        return
+
+    current_card = random.choice(to_learn.iloc[:, 0].tolist())
     if 30 < len(current_card) < 20:
         font_size = 40
     else:
@@ -58,6 +68,19 @@ def is_known():
     next_card()
 
 
+def restart_program():
+    # Reload the original words from the CSV and start over
+    global to_learn
+    to_learn = original_data
+    next_card()
+    known_button.grid(row=1, column=2)
+    unknown_button.grid(row=1, column=0)
+
+
+def close_program():
+    window.destroy()
+
+
 window = Tk()
 window.title("Nursing Flash Card")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -69,10 +92,11 @@ card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+completed_card_word = canvas.create_text(400, 263, text="", width=700)
 front_card_word = canvas.create_text(400, 263, text="", width=700)
 def_card_word = canvas.create_text(400, 263, text="", width=700)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.grid(row=0, column=0, columnspan=2)
+canvas.grid(row=0, column=0, columnspan=3)
 
 cross_image = PhotoImage(file="images/wrong.png")
 unknown_button = Button(image=cross_image, highlightthickness=0, command=next_card)
@@ -80,7 +104,11 @@ unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
 known_button = Button(image=check_image, highlightthickness=0, command=is_known)
-known_button.grid(row=1, column=1)
+known_button.grid(row=1, column=2)
+
+restart_image = PhotoImage(file="images/restart.png")
+restart_button = Button(image=restart_image, highlightthickness=0, command=restart_program)
+restart_button.grid(row=1, column=1)
 
 next_card()
 
